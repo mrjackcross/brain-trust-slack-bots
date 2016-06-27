acfc74a4ce9d80519836afeed27d8ac1e83a21e0
mrjackcross <jcross@omnifone.com>
2016-06-27 15:13:30 +0100

    added debug logging

--- the-brain.py
+++ the-brain.py
@@ -99,7 +99,9 @@ if __name__ == "__main__":
     if slack_client.rtm_connect():
         print("TheBrain connected and running!")
         parse_slack_history()
-        while True:
+        print("Parsed today's history!")
+        print(user_arrive_times)
+    while True:
             command, user, channel, ts = parse_slack_output(slack_client.rtm_read())
             if command and channel and ts:
                 handle_command(command, user, channel, ts)
